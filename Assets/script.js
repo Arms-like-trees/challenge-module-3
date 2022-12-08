1aaea450a3554e38e186211e5e98878656736c1d
Arms-like-trees <117699558+Arms-like-trees@users.noreply.github.com>
2022-12-07 19:50:37 -0600

    made function generatePassword and started to flesh it out

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//function for gereratePassword
+
+function gereratePassword(){
+  //ask for length
+  var length = prompt("How long would you like the password length(8-128)?")
+  //validate that length is numeric 8-128
+
+  //ask for lowercase
+  var lowerCase = prompt("Do you want lowercase(Y/N)?" ) 
+  //ask for uppercase
+  var uppercase = prompt("Do you want uppercase?");
+  //ask for numeric
+  var numbers = prompt("Do you want numbers?");
+  //ask for special criteria
+  var specialCharacters = prompt("Do you want special characters?");
+  //validate that atleast one of above four criteria (lowercase, uppercase, numeric, symbol).
+
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
